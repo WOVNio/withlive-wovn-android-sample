717d9c14434b046b1fa5c91047f9206cd809e67a
you12724 <you12724@gmail.com>
2023-09-26 20:30:19 +0900

    feat: sample

--- app/src/main/java/com/example/wovnsample/MainApplication.kt
+++ app/src/main/java/com/example/wovnsample/MainApplication.kt
@@ -1,6 +1,11 @@
+package com.example.wovnsample
+
+import android.app.Application
+import io.wovn.wovnapp.WovnActivityLifecycleCallbacks
+
 class MainApplication: Application() {
     override fun onCreate() {
-        Log.d("TEST=----------------", "テストだよ")
+        this.registerActivityLifecycleCallbacks(WovnActivityLifecycleCallbacks())
         super.onCreate()
     }
 }
